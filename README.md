4c9f1be7e64ccb663c07578ff9c67114fe81dfdc
ddivernois <david-emmanuel.divernois@amadeus.com>
2013-03-04 16:10:36 +0100

    feat #1 initial import
    Closes #1

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 atpackager
 ==========
 
-The Aria templates packager
+[![Build Status](https://secure.travis-ci.org/ariatemplates/atpackager.png?branch=master)](http://travis-ci.org/ariatemplates/atpackager)
+
+atpackager is a [Grunt](http://gruntjs.com) 0.4.x plugin to create a set of packages from a set of input files,
+doing transformations in the middle.
+
+It is developed primarily to package the [Aria Templates framework](http://ariatemplates.com/) itself, and applications
+built with Aria Templates. However, it can also be used as a build tool independently of Aria Templates.
+
+It is based on the principle of visitors, allowing to easily add custom transformations to files.
---
+++ tasks/task-atpackager.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = function (grunt) {
+    require('../lib/grunt').init(grunt);
+
+    var Packaging = require('../lib/packager/packaging');
+
+    grunt.registerMultiTask('atpackager', 'Aria Templates packager', function () {
+        try {
+            var currentPackaging = new Packaging();
+
+            var options = this.options({
+                sourceDirectories : [],
+                sourceFiles : [],
+                outputDirectory : 'output',
+                visitors : [],
+                packages : [],
+                defaultBuilder : null,
+                ATBootstrapFile : 'aria/bootstrap.js'
+            });
+
+            currentPackaging.ATBootstrapFile = options.ATBootstrapFile;
+            currentPackaging.defaultBuilder = options.defaultBuilder;
+            currentPackaging.sourceDirectories = options.sourceDirectories;
+            currentPackaging.outputDirectory = options.outputDirectory;
+            currentPackaging.addVisitors(options.visitors);
+            currentPackaging.addSourceFiles(options.sourceFiles);
+            currentPackaging.addPackages(options.packages);
+            currentPackaging.build();
+        } catch (e) {
+            grunt.log.error(e);
+            if (e.stack && grunt.option('stack')) {
+                console.log(e.stack);
+            }
+            grunt.warn("An error interrupted atpackager.");
+            return false;
+        }
+
+        if (this.errorCount > 0) {
+            grunt.warn("There were " + this.errorCount + " error(s) while packaging.");
+            return false;
+        }
+
+        grunt.log.ok();
+    });
+};
